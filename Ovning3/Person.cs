37819bc3f758af3bfe3a15f8298ceb2932ca5737
jarethholt <jareth.holt@gmail.com>
2024-03-07 10:42:41 +0100

    feat: Implement Person class
    Implement the Person class according to problem 1.1. Add methods to Main that show off invalid constructors.

---
+++ Ovning3/Person.cs
@@ -0,0 +1,80 @@
+﻿namespace Ovning3;
+
+/* 1. Skapa en klass Person och ge den följande privata fält:
+age, fName, lName, height, weight
+Skapa publika properties med get och set som hämtar eller sätter tilldelad variabel.
+Instansiera en person i Program.cs, kommer du direkt åt variablerna?
+Implementera validering i de skapade properties:
+● Age kan bara tilldelas ett värde större än 0.
+● FName är obligatorisk och får inte vara mindre än 2 tecken eller längre än 10
+tecken.
+● LName är obligatorisk och får inte vara mindre än 3 tecken eller större än 15
+tecken.
+Kasta ett undantag av typen ArgumentException i varje property om dess
+validering inte fullföljs, undantaget ska innehålla ett beskrivande
+meddelande.
+Se till att hantera undantagen i Program-klassen med en try-catch block.
+ */
+
+internal class Person
+{
+    private int age;
+    public int Age
+    {
+        get => age;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("The age must be greater than 0");
+            age = value;
+        }
+    }
+
+    private string fName;
+    public string FName
+    {
+        get => fName;
+        set
+        {
+            if (value.Length < 2 || value.Length > 10)
+                throw new ArgumentException("The first name must be between 2 and 10 characters");
+            fName = value;
+        }
+    }
+
+    private string lName;
+    public string LName
+    {
+        get => lName;
+        set
+        {
+            if (value.Length < 3 || value.Length > 15)
+                throw new ArgumentException("The last name must be between 3 and 15 characters");
+            lName = value;
+        }
+    }
+
+    private decimal height;
+    public decimal Height
+    {
+        get => height;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("The height must be greater than 0");
+            height = value;
+        }
+    }
+
+    private decimal weight;
+    public decimal Weight
+    {
+        get => weight;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("The weight must be greater than 0");
+            weight = value;
+        }
+    }
+}
--- Ovning3/Program.cs
+++ Ovning3/Program.cs
@@ -1,10 +1,124 @@
-﻿namespace Ovning3
+﻿namespace Ovning3;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        TryGetPrivateProperties();
+        TryInvalidPersonParameters();
+    }
+
+    readonly record struct PersonParams(int Age, string FName, string LName, decimal Height, decimal Weight)
+    {
+        public Person CreatePerson() => new()
         {
-            Console.WriteLine("Hello, World!");
+            Age = Age,
+            FName = FName,
+            LName = LName,
+            Height = Height,
+            Weight = Weight
+        };
+    }
+
+    static void TryGetPrivateProperties()
+    {
+        Person person = new()
+        {
+            Age = 18,
+            FName = "Sam",
+            LName = "Smith",
+            Height = 160m,
+            Weight = 70m
+        };
+
+        /*
+        int age = person.age;
+        // CS0122: 'Person.age' is inaccessible due to its protection level
+        Console.WriteLine($"The Person's private age is {age}");
+        */
+
+        Console.WriteLine($"The Person's public age is {person.Age}");
+        Console.WriteLine();
+    }
+
+    static void TryInvalidPersonParameters()
+    {
+        PersonParams okayParams = new(18, "Sam", "Smith", 160m, 70m);
+        Person validPerson = okayParams.CreatePerson();
+
+        int[] badAges = [0];
+        string[] badFNames = ["A", "Atrociousness"];
+        string[] badLNames = ["Ng", "Expialodociousness"];
+        decimal[] badHeights = [0m];
+        decimal[] badWeights = [0m];
+
+        Console.WriteLine("Examples of invalid parameters for Person:");
+
+        foreach (int age in badAges)
+        {
+            try
+            {
+                var invalidParams = okayParams with { Age = age };
+                Person invalidPerson = invalidParams.CreatePerson();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
+
+        foreach (string fName in badFNames)
+        {
+            try
+            {
+                var invalidParams = okayParams with { FName = fName };
+                Person invalidPerson = invalidParams.CreatePerson();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        foreach (string lName in badLNames)
+        {
+            try
+            {
+                var invalidParams = okayParams with { LName = lName };
+                Person invalidPerson = invalidParams.CreatePerson();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        foreach (decimal height in badHeights)
+        {
+            try
+            {
+                var invalidParams = okayParams with { Height = height };
+                Person invalidPerson = invalidParams.CreatePerson();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        foreach (decimal weight in badWeights)
+        {
+            try
+            {
+                var invalidParams = okayParams with { Weight = weight };
+                Person invalidPerson = invalidParams.CreatePerson();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        Console.WriteLine();
     }
 }
