e6db83c4f94caad8267af1fd6347335f06de71e4
jarethholt <jareth.holt@gmail.com>
2024-03-08 11:43:04 +0100

    refactor: Move PersonHandler into Person.cs

--- Ovning3/Del1/Person.cs
+++ Ovning3/Del1/Person.cs
@@ -64,3 +64,24 @@ internal class Person
         }
     }
 }
+
+
+internal class PersonHandler
+{
+    public static Person CreatePerson(
+        int age, string fName, string lName, decimal height, decimal weight
+    ) => new()
+    {
+        Age = age,
+        FName = fName,
+        LName = lName,
+        Height = height,
+        Weight = weight
+    };
+
+    public static void SetAge(Person person, int age) => person.Age = age;
+    public static void SetFirstName(Person person, string fName) => person.FName = fName;
+    public static void SetLastName(Person person, string lName) => person.LName = lName;
+    public static void SetHeight(Person person, decimal height) => person.Height = height;
+    public static void SetWeight(Person person, decimal weight) => person.Weight = weight;
+}
